8d07fabd01dba98cdb027b92ad8c593ad47b2fc7
PrithvijRajesh <prithvijr@gmail.com>
2024-05-14 10:17:21 -0700

    Initial Commit

---
+++ Rate my class test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rate my class test
+//
+//  Created by student on 5/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Rate my class testTests/Rate_my_class_testTests.swift
@@ -0,0 +1,33 @@
+//
+//  Rate_my_class_testTests.swift
+//  Rate my class testTests
+//
+//  Created by student on 5/14/24.
+//
+
+import XCTest
+@testable import Rate_my_class_test
+
+class Rate_my_class_testTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
